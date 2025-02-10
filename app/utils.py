3b5ccdeae1d8abbd3139a7fa2c760a78923928b4
kgnp684 <kgnp684@astrazeneca.net>
2025-02-10 22:16:31 +0530

    minor changes for validation key

--- app/utils.py
+++ app/utils.py
@@ -176,11 +176,21 @@ def extract_text_from_word(byte_array):
     except Exception as e:
          raise HTTPException(status_code=400, detail=f"Error reading file: {str(e)}")
             
-def validate_api_key(apiKey: str):
-    try:       
-        if not apiKey or apiKey != API_KEY:
-            return True
-        return False
+# def validate_api_key(apiKey: str):
+#     try:       
+#         if not apiKey or apiKey != API_KEY:
+#             return True
+#         return False
+#     except Exception as e:
+#         print(str(e))
+#         raise HTTPException(status_code=500, detail=f"Authentication verification failed {str(e)}")
+    
+def validate_api_key(apiKey: str) -> bool:
+    try:
+        if apiKey and apiKey == API_KEY: 
+            return True  
+        else:
+            return False 
     except Exception as e:
         print(str(e))
-        raise HTTPException(status_code=500, detail=f"Authentication verification failed {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Authentication verification failed: {str(e)}")
